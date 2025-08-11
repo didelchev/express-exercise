4cdf733a0ec4d874b7e58e20c27f8dad66f3d36b
Daniel Delchev <daniel.delchev@yahoo.com>
2025-08-11 23:05:15 +0300

    fixed href paths

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,9 +2,36 @@ import { Router  } from "express";
 
 const productController = Router();
 
+
+// HOME 
 productController.get("/", (req, res) => {
-    res.send('<h1>Products wokrs</h1>')
+    res.render('product/catalog', {title: "Catalog Page"})
 })
 
+
+// CREATE
+productController.get("/create", (req, res) => {
+    res.render('product/create', {title: "Create a Product"})
+})
+
+
+
+// DETAILS
+productController.get("/:productId/details", (req,res) => {
+    const productId = req.params.productId
+    console.log(productId)
+    res.render('product/details', {title: "Details Page"})
+})
+
+productController.get("/details/edit", (req,res) => {
+    res.render('product/edit', {title: "Edit Page"})
+})
+
+
+
+// SEARCH
+productController.get("/search", (req,res) => {
+    res.render('product/search', {title: "Search Page"})
+})
 
 export default productController
